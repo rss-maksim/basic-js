d9dc686cbd75499154d736086464dc62f7e1d450
Maksim Kirilko <maksim.kirilko@skywindgroup.com>
2020-03-12 15:14:09 +0300

    feat: implement transform-array and what season

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,39 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+const sequencesMapHandlers = {
+    '--discard-next': discardNext,
+    '--discard-prev': discardPrev,
+    '--double-next': doubleNext,
+    '--double-prev': doublePrev
 };
+
+const set = new Set(Object.keys(sequencesMapHandlers));
+
+module.exports = function transform(array) {
+    if (!Array.isArray(array)) {
+        throw new Error('Array not presented')
+    }
+    if (!array.length) {
+        return array;
+    }
+    for (let i = 0; i < array.length; i++) {
+        const item = array[i];
+        if (set.has(item)) {
+            return sequencesMapHandlers[item](array.filter(item => !set.has(item)), i);
+        }
+    }
+};
+
+function discardNext(array, i) {
+    return array.filter((item, index) => index !== i + 1);
+}
+
+function discardPrev(array, i) {
+    return array.filter((item, index) => index !== i - 1);
+}
+
+function doubleNext(array, i) {
+    return [...array.slice(0, i), array[i + 1], ...array.slice(i + 1)];
+}
+
+function doublePrev(array, i) {
+    return [...array.slice(0, i - 1), array[i - 1], ...array.slice(i - 1)];
+}
--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,26 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+function isValid(date) {
+  return date instanceof Date && !isNaN(date);
+}
+
+const seasons = [
+  [11, 'winter'],
+  [8, 'autumn'],
+  [5, 'summer'],
+  [2, 'spring'],
+  [0, 'winter'],
+];
+
+module.exports = function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+  if (!isValid(date)) {
+    throw new Error('Invalid date');
+  }
+  const month = date.getMonth();
+  for (let [key, season] of seasons) {
+    if (month >= key) {
+      return season;
+    }
+  }
 };
