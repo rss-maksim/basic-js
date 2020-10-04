68dbcdd330b81e498137a1af1d17ede1a5872da1
Maksim Kirilko <maksimkirilko@gmail.com>
2020-10-04 17:30:08 +0300

    Add recursive depth solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,4 +1,13 @@
 module.exports = function repeater(str, options) {
-    const { repeatTimes, separator, addition, additionRepeatTimes, additionSeparator } = options
-    return
+    const { repeatTimes = 1, separator = '+', addition, additionRepeatTimes = 1, additionSeparator = '|' } = options;
+    const additionString = repeat(addition, additionRepeatTimes, additionSeparator);
+    if (!str) {
+        console.log('repeater', { addition, additionString })
+    }
+    return repeat(`${str}${additionString}`, repeatTimes, separator);
 };
+
+function repeat(str, n, separator) {
+    const string = typeof str === 'undefined' ? '' : String(str);
+    return Array.from({ length: n }, () => string).join(separator);
+}
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,18 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr) {
+        let depth = 1;
+        while (this.isArrayIncludes(arr)) {
+            depth += 1;
+            arr = this.flat(arr);
+        }
+        return depth;
+    }
+
+    isArrayIncludes(arr) {
+        return arr.some(item => Array.isArray(item));
+    }
+
+    flat(arr) {
+        return arr.reduce((acc, item) => [...acc,  ...(Array.isArray(item) ? item : [item])], []);
     }
 };
